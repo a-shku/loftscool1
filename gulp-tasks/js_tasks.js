7a5a49d72385d88a53175e086287694dc80976e2
Andrey <vsenetak88@mail.ru>
2017-04-10 15:22:24 +0000

    packacgeJson

--- gulp-tasks/js_tasks.js
+++ gulp-tasks/js_tasks.js
@@ -1,3 +1,13 @@
 'use strict';
 
 var gulp = require('gulp'),
+    plugins = require("gulp-load-plugins")({
+        pattern: ['gulp-*', 'gulp.*'],
+        replaceString: /\bgulp[\-.]/
+    });
+
+gulp.task('less', function(){
+    gulp.src('web/core/assets/less/*.less')
+    .pipe(plugins.less())
+    .pipe.gulp.dest('web/core/assets/css');
+});
---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+'use strict';
+
+var gulp = require('gulp');
+
+// include all tasks from separated files (./tasks)
+require('require-dir')('./gulp-tasks');
