a79ba4020bbbe1f3ae61a87c2b33d4f322b11dfd
Andrey <vsenetak88@mail.ru>
2017-04-15 13:59:57 +0000

    plugged fontawesome & gulpTask for assetsCSS

--- gulp-tasks/style.task.js
+++ gulp-tasks/style.task.js
@@ -7,15 +7,32 @@ var gulp = require('gulp'),
     });
     
     
-var paths = {};
+var resources = {}, paths = {}, assetsFiles;
 
 paths.bowerComponents = 'bower_components/';
+
+resources = {
+    css: [
+        paths.bowerComponents + 'font-awesome/css/font-awesome.css'
+    ]
+};
 
 gulp.task('less', function(){
    return gulp.src('web/core/assets/less/_common-style.less')
     .pipe(plugins.less())
     .pipe(plugins.rename('main-app.css'))
     .pipe(gulp.dest('web/core/assets'));
+});
+
+
+gulp.task('css:assets', function () {
+    return gulp.src(resources.css)
+        .pipe(plugins.concat('assets.min.css'))
+        .pipe(plugins.size({
+            showFiles: true
+        }))
+        .pipe(gulp.dest('web/vendor/'));
+      
 });
 
 gulp.task('default', ['less']);
