6fd46a9ed732ea375715e295ecbdda0b4f866dc6
Andrey <vsenetak88@mail.ru>
2017-04-15 13:01:57 +0000

    bower depends & fontawesome

--- gulp-tasks/style.task.js
+++ gulp-tasks/style.task.js
@@ -1,2 +1,21 @@
 'use strict';
 
+var gulp = require('gulp'),
+    plugins = require("gulp-load-plugins")({
+        pattern: ['gulp-*', 'gulp.*'],
+        replaceString: /\bgulp[\-.]/
+    });
+    
+    
+var paths = {};
+
+paths.bowerComponents = 'bower_components/';
+
+gulp.task('less', function(){
+   return gulp.src('web/core/assets/less/_common-style.less')
+    .pipe(plugins.less())
+    .pipe(plugins.rename('main-app.css'))
+    .pipe(gulp.dest('web/core/assets'));
+});
+
+gulp.task('default', ['less']);
