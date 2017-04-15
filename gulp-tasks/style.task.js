0cfaeb7fd7caf2b08b8ffe80ce2988482f8deec7
Andrey <vsenetak88@mail.ru>
2017-04-15 17:25:22 +0000

    css task

--- gulp-tasks/style.task.js
+++ gulp-tasks/style.task.js
@@ -10,10 +10,16 @@ var gulp = require('gulp'),
 var resources = {}, paths = {}, assetsFiles;
 
 paths.bowerComponents = 'bower_components/';
+paths.css = 'web/core/assets/css';
 
 resources = {
     css: [
-        paths.bowerComponents + 'font-awesome/css/font-awesome.css'
+        paths.css + 'reset.css'
+        ,paths.css + 'fonts.css'
+        ,paths.css + 'common-style.css'
+    ],
+    assestCss: [
+        paths.bowerComponents + 'font-awesome/css/font-awesome.min.css'
     ]
 };
 
@@ -24,14 +30,21 @@ gulp.task('less', function(){
     .pipe(gulp.dest('web/core/assets'));
 });
 
+gulp.task('css', function(){
+  return gulp.src(resources.css)
+    
+    .pipe(plugins.concat('main-app.css'))
+    .pipe(gulp.dest('web/core/assets'));
+});
+
 
 gulp.task('css:assets', function () {
-    return gulp.src(resources.css)
+    return gulp.src(resources.assestCss)
         .pipe(plugins.concat('assets.min.css'))
         .pipe(plugins.size({
             showFiles: true
         }))
-        .pipe(gulp.dest('web/vendor/'));
+        .pipe(gulp.dest('web/vendor/css'));
       
 });
 
