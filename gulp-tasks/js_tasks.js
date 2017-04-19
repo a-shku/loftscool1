953c10ceefe45d1de75f9368b27fba7f46517f77
Andrey <vsenetak88@mail.ru>
2017-04-19 20:33:51 +0000

    task for own js

--- gulp-tasks/js_tasks.js
+++ gulp-tasks/js_tasks.js
@@ -25,7 +25,6 @@ gulp.task('clean:js:assets', function () {
 });
 
 gulp.task('js:assets', ['clean:js:assets'], function () {
-    console.log('resources.bowerJs', resources.bowerJs);
     
     return  gulp.src(resources.bowerJs)
             .pipe(plugins.concat('web/vendor/js/**/*.js'))
@@ -36,4 +35,19 @@ gulp.task('js:assets', ['clean:js:assets'], function () {
         }))
         .pipe(gulp.dest('web/vendor/js/bundle/'));
         
+});
+
+gulp.task('clean:js:app', function () {
+    return del.sync(['web/core/js/main-app.js']);
+});
+
+gulp.task('js:app', ['clean:js:app'], function() {
+    console.log('resources.bowerJs', resources.bowerJs);
+
+    return gulp.src('web/core/assets/js/**/*js')
+        .pipe(plugins.concat('main-app.js'))
+        .pipe(plugins.size({
+            showFiles: true
+        }))
+        .pipe(gulp.dest('web/core/assets/'));
 });
---
+++ web/core/assets/main-app.js
@@ -0,0 +1,4 @@
+ $(document).ready(function() {
+            var a = $('body');
+            console.log('included');
+});
