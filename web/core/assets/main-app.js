c32ac9e1401c6389e4075d0d7d6723189872aee5
Andrey <vsenetak88@mail.ru>
2017-04-22 17:44:35 +0000

    routing

--- web/core/assets/main-app.js
+++ web/core/assets/main-app.js
@@ -1,56 +1,81 @@
  $(document).ready(function() {
-            var a = $('body');
-            console.log('included');
-            
-           
+          
             
 });
   $(document).ready(function() {
       
-      var states = {
-          about: {
-              url: '/web/core/pages/about.html',
-              name: 'about'
-          },
-          career: {
-              url: '/web/core/pages/career.html',
-              name: 'carrer'
-          },
-          portfolio: {
-              url: '/web/core/pages/portfolio.html',
-              name: 'portfolio'
+    var hash = window.location.hash.substr(1);
+    
+      
+    window.onhashchange = changeUrlHandler;
+      
+    var states = {
+        about: {
+            url: '/web/core/pages/about.html',
+            name: 'about'
+        },
+        career: {
+            url: '/web/core/pages/career.html',
+            name: 'career'
+        },
+        portfolio: {
+            url: '/web/core/pages/portfolio.html',
+            name: 'portfolio'
+        }
+    };
+    
+    function changeUrlHandler() {
+      console.log('134', window.location.hash.substr(1));
+      hash = window.location.hash;
+      if(hash.charAt(0) == '#'){
+          hash = window.location.hash.substr(1);
+      }
+      
+    
+      if (hash) {
+          try {
+              setContent(hash);
           }
-      };
+          catch (error) {
+              console.log(error);
+          }
+      }
+    
+    };
+    
+      
+    if(hash){
+        try {
+            setContent(hash);
+        } catch(error){console.log(error);}
+        
+    }
       
       
-          var contentContainer = $('#content-container');
+      
+    var contentContainer = $('#content-container');
  
-    console.log('router', $('.menu__item__link'));
             
-            var setContent = function(file){
-                console.log('file', file, states[file].url);
-                //var hash = 
-                 window.location.hash = states[file].name;
-                $.get(states[file].url, function(data) {
-                    
-                    contentContainer.html(data);
-                   //$(this).children("div:first").html(data);
-                });
-            };
+    function setContent(file){
+        
+        //console.log('file', file,  states[file].url);
+        
+         window.location.hash = states[file].name;
+         
+        $.get(states[file].url, function(data) {
+            contentContainer.html(data);
+        });
+    };
             
-            var menuLink = $('.menu__item__link');
-            
-            console.log('menuLink', menuLink);
-            
-           menuLink.on( "click" , function(e){
-              
-               e.preventDefault();
-              //console.log($(this).attr("class"));
-              console.log($(this).attr("href"));
-              var url = $(this).attr("href");
-              
-               setContent(url);
-           });  
+    var menuLink = $('.menu__item__link');
+
+    menuLink.on("click", function(e) {
+        e.preventDefault();
+        
+        var url = $(this).attr("href");
+
+        setContent(url);
+    });
             
            
             
