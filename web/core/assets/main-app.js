b0753e883956e473842df5d386b5642551e68e4e
Andrey <vsenetak88@mail.ru>
2017-05-08 15:44:50 +0000

    entryPage

--- web/core/assets/main-app.js
+++ web/core/assets/main-app.js
@@ -6,11 +6,11 @@ $(document).ready(
 
   function() { 
 
-    $("html").niceScroll({
-        cursorcolor: "#d8d8d8",
-        cursorwidth: "10px",
-        hidecursordelay: "800"
-    });
+    // $("html").niceScroll({
+    //     cursorcolor: "#d8d8d8",
+    //     cursorwidth: "10px",
+    //     hidecursordelay: "800"
+    // });
    
     
 
